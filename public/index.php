1ecb6814895fdd160d3ac630a134a7c3fa810051
John Coggeshall <john@coggeshall.org>
2014-02-05 14:42:02 -0500

    Adding some environment stuff

--- public/index.php
+++ public/index.php
@@ -5,6 +5,10 @@
  */
 chdir(dirname(__DIR__));
 
+if(!defined("APPLICATION_ROOT")) {
+    define("APPLICATION_ROOT", dirname(__DIR__));
+}
+
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
@@ -14,6 +18,11 @@ if(isset($_SERVER['APPLICATION_ENV']) && !defined('APPLICATION_ENV')) {
     define('APPLICATION_ENV', $_SERVER['APPLICATION_ENV']);
 } else if(!defined('APPLICATION_ENV')) {
     define('APPLICATION_ENV', 'development');
+}
+
+if (APPLICATION_ENV == 'development') {
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
 }
 
 // Setup autoloading
