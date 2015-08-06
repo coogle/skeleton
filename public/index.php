7b32a922f1b0a74cddd4c93d386a565a9c6ccb8c
John Coggeshall <john@coggeshall.org>
2015-08-06 15:40:46 -0400

    Implementing a Skeleton for Wordpress (4.2.4)

--- public/index.php
+++ public/index.php
@@ -1,49 +1,17 @@
 <?php
 /**
- * Laravel - A PHP Framework For Web Artisans
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * @package  Laravel
- * @author   Taylor Otwell <taylorotwell@gmail.com>
+ * @package WordPress
  */
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader
-| for our application. We just need to utilize it! We'll require it
-| into the script here so that we do not have to worry about the
-| loading of any our classes "manually". Feels great to relax.
-|
-*/
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-require __DIR__.'/../bootstrap/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let's turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight these users.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/start.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can simply call the run method,
-| which will execute the request and send the response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have whipped up for them.
-|
-*/
-
-$app->run();
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
--- puppet/modules/app/manifests/database.pp
+++ puppet/modules/app/manifests/database.pp
@@ -1,7 +1,13 @@
 class app::database {
 
   class { '::mysql::server' :
-     root_password => 'password'
+    root_password => 'password',
+    databases => {
+        'wordpress' => {
+     	    ensure => 'present',
+	    charset => 'utf8'
+    	}
+    }
   }
-   
+
 }
